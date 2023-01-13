407063960a8c0edbac66dbb3f99e3ac50781a468
ivaandiaz <ivan.diazfernandez@iesmiguelherrero.com>
2023-01-13 17:44:31 +0100

    he realizado el programa pedido

---
+++ ejercicio3/src/main/java/ejercicio3/ejercicio3/Ejercicio3.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ejercicio3.ejercicio3;
+
+/**
+ *
+ * @author DAM110
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
